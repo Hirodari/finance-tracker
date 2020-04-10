8b2073e9b10fbe4a598d106dfe2b6baf95c96d0c
Fred Bitenyo <fredbitenyo@gmail.com>
2020-04-10 15:00:24 +0200

    create stock model, class method to lookup info and secure credentials

---
+++ app/models/stock.rb
@@ -0,0 +1,7 @@
+class Stock < ApplicationRecord
+  def self.new_lookup(sticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+    secret_token: 'secret_token', endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(sticker_symbol)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_170304) do
+ActiveRecord::Schema.define(version: 2020_04_10_115447) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name"
+    t.string "ticker"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
